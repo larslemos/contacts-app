14d468c576b4a37d72b6ec625d4fe5be291cf08c
larslemos <larslemos@gmail.com>
2019-10-15 10:45:41 +0200

    Initial Contact list set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import ListContacts from './ListContacts.js'
+
+const contacts = [
+ {
+   "id": "karen",
+   "name": "Karen Isgrigg",
+   "handle": "karen_isgrigg",
+   "avatarURL": "http://localhost:5001/karen.jpg"
+ },
+ {
+   "id": "richard",
+   "name": "Richard Kalehoff",
+   "handle": "richardkalehoff",
+   "avatarURL": "http://localhost:5001/richard.jpg"
+ },
+ {
+   "id": "tyler",
+   "name": "Tyler McGinnis",
+   "handle": "tylermcginnis",
+   "avatarURL": "http://localhost:5001/tyler.jpg"
+ }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <ListContacts contacts={contacts} />
     </div>
   );
 }
